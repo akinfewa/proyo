2bc5f5378c80e6bfc936cf3c96efb60fba8772a7
laridsen <50364703+laridsen@users.noreply.github.com>
2019-06-03 13:47:17 +0200

    test

--- proyo/contract/src/main/java/contract/IAlive.java
+++ proyo/contract/src/main/java/contract/IAlive.java
@@ -2,6 +2,13 @@ package contract;
 
 import entity.Direction;
 
+/**
+ *  The Ialive who define the position.
+ * @author Damien
+ * @param direction
+ * 				Allow to manage a direction of an object. 
+ */
+
 public interface IAlive {
 	public void xMove(Direction direction);
 	public void yMove(Direction direction);
